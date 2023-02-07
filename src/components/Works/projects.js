199922cdb4773a3b44323e307c894c585f6d6d2d
MarcosAF9 <marcos_falcade1@hotmail.com>
2023-02-07 15:47:54 -0300

    projeto de consulta de Cep adicionado

--- src/components/Works/projects.js
+++ src/components/Works/projects.js
@@ -3,6 +3,7 @@ import img2 from '../../assets/siteParallax.png';
 import img3 from '../../assets/cronometro.png';
 import img4 from '../../assets/paginaLogin.png';
 import img5 from '../../assets/nlwEsports.png';
+import img6 from '../../assets/consulta-cep.png';
 
 export const projects = [
     {
@@ -39,5 +40,12 @@ export const projects = [
         "linkCodigo": "https://github.com/MarcosAF9/NLW-eSports",
         "image": img5,
         "id": 5
+    },
+    {
+        "title": "Consulta de CEP usando fetch API",
+        "linkProjeto": "https://consulta-de-cep-ten.vercel.app/",
+        "linkCodigo": "https://github.com/MarcosAF9/consulta-de-cep",
+        "image": img6,
+        "id": 6
     }
 ]
