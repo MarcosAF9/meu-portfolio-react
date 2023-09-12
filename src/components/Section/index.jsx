4f2243a3ee1c586a4813a3a038dfd69f67bbf9b4
MarcosAF9 <marcos_falcade1@hotmail.com>
2023-09-12 15:53:48 -0300

    Tecnologias adicionadas

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,24 +1,26 @@
-import foto from '../../assets/foto-perfil.jpg';
-import styles from './Section.module.css';
+import foto from "../../assets/foto-perfil.jpg";
+import styles from "./Section.module.css";
 
 function Section() {
-    return (
-        <div className={styles.section} id="technologies">
-            <div className={styles.container}>
-                <div className={styles.image}>
-                    <img src={foto} alt="minha foto" />
-                </div>
-                <div className={styles.content}>
-                    <h2>Tecnologias que estudo</h2>
-                    <p>Javascript</p>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React</p>
-                    <p>GIT</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.section} id="technologies">
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <img src={foto} alt="minha foto" />
         </div>
-    )
+        <div className={styles.content}>
+          <h2>Tecnologias que estudo</h2>
+          <p>Javascript</p>
+          <p>Nextjs</p>
+          <p>NodeJs</p>
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>React</p>
+          <p>GIT</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Section;
